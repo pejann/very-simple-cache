ebc49977d1a1b14abbb3494801471bb5dc1b0bf0
pejann <pedrojnt@gmail.com>
2019-04-12 21:04:47 -0300

    Removing time constrains

--- test/helper/cache-data/blankCacheData.spec.js
+++ test/helper/cache-data/blankCacheData.spec.js
@@ -4,6 +4,7 @@ const { blankCacheData } = require('../../../src/helper/cache-data')
 describe('Cache Data', async () => {
   describe('blankCacheData', () => {
     it('should return a blank CacheData-like object', () => {
+
       expect(blankCacheData()).to.be.a('object')
         .to.have.property('unixExpirationDate')
         .to.equal(null)
